386ad535d24458d3f4119536159e12fd09837730
Pramod <Pramod@PRAMOD-PC>
2020-09-10 12:57:50 +0530

    Delet file

--- src/Redux/Actions.js
+++
@@ -1,8 +0,0 @@
-import {ADD_ENCOUNTER } from "./ActionTypes"
-
-export const addEncounter = (encounterData) => {
-    return {
-        type: ADD_ENCOUNTER,
-        data: encounterData
-    }
-}
--- src/Redux/Store.js
+++
@@ -1,4 +0,0 @@
-import { createStore } from "redux";
-import { commonReducer } from "./Reducer";
-
-export const store = createStore(commonReducer)
