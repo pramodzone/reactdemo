e2ce0df599e9c9d70f9a69a8444c241cb2ed7233
Pramod <Pramod@PRAMOD-PC>
2020-09-10 13:02:25 +0530

    push files

---
+++ precache-manifest.7484b3387c260af26cf6e8d5dda4bbad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ce10a79e2f0970b1021311e0b4a3eff",
+    "url": "/reactdemo/index.html"
+  },
+  {
+    "revision": "3a13c937373d35692e1b",
+    "url": "/reactdemo/static/css/main.31c27506.chunk.css"
+  },
+  {
+    "revision": "5980a573da024fb1c03c",
+    "url": "/reactdemo/static/js/2.a4b4118f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactdemo/static/js/2.a4b4118f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a13c937373d35692e1b",
+    "url": "/reactdemo/static/js/main.c64b373e.chunk.js"
+  },
+  {
+    "revision": "d342771f319a1eefbd16",
+    "url": "/reactdemo/static/js/runtime-main.50b03081.js"
+  }
+]);
