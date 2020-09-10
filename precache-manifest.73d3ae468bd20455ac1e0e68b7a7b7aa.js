7360e7196b544464a3565427960aa2cb83d00a34
Pramod <Pramod@PRAMOD-PC>
2020-09-10 12:58:51 +0530

    pusing build file only

---
+++ precache-manifest.73d3ae468bd20455ac1e0e68b7a7b7aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28a42ce1208286db0b72df9457c47746",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f86563ea8ce6d02721ca",
+    "url": "/static/css/main.31c27506.chunk.css"
+  },
+  {
+    "revision": "d19eb091a81fd250626b",
+    "url": "/static/js/2.54e9fc08.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.54e9fc08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f86563ea8ce6d02721ca",
+    "url": "/static/js/main.a53f1f85.chunk.js"
+  },
+  {
+    "revision": "05fd983a113f2be2e0a6",
+    "url": "/static/js/runtime-main.bd60c0be.js"
+  }
+]);
